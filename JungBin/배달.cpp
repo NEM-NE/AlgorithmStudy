dc870cbad8bb6114c175dafead8eb8630759c7e4
whwjdqls <73946308+whwjdqls@users.noreply.github.com>
2021-06-01 09:36:47 +0900

    Update 배달.cpp

--- JungBin/배달.cpp
+++ JungBin/배달.cpp
@@ -72,3 +72,63 @@ int solution(int N, vector<vector<int> > road, int K) {
  }
     return answer;
 }
+
+
+#include <iostream>
+#include <vector>
+#include<queue>
+#include<utility>
+#include<cstring>
+#include<limits>
+#include<utility>
+#include<unordered_map>
+
+using namespace std;
+int infi=numeric_limits<int>::max();
+int dis[51];
+
+unordered_map<int,vector<pair<int,int>>> neighbors;
+
+int solution(int N, vector<vector<int> > road, int K) {
+    int answer = 0;
+    memset(dis,infi,sizeof(dis));
+    dis[1]=0;
+    for(int i=0;i<road.size()-1;i++)
+    {
+     pair<int,int> edge0=make_pair(road[i][0],road[i][2]);
+     pair<int,int> edge1=make_pair(road[i][1],road[i][2]);
+        if(!neighbors.count(road[i][0]))
+        {
+            vector<pair<int,int>> v;
+            neighbors[road[i][0]]=v;
+            }
+        if(!neighbors.count(road[i][1]))
+        {
+            vector<pair<int,int>> v;
+            neighbors[road[i][1]]=v;
+            }
+        neighbors[road[i][0]].push_back(edge1);
+        neighbors[road[i][1]].push_back(edge0);
+    }
+    
+queue<int> q;
+    q.push(1);
+    while(!q.empty())
+    {
+        int cur=q.front();
+        q.pop();
+        if(dis[cur]<=K)
+            answer++;
+        
+        for(auto neighbor : neighbors[cur])
+        {
+            if(dis[cur]+neighbor.second<dis[neighbor.first])
+            {
+                q.push(neighbor.first);
+                dis[neighbor.first]=dis[cur]+neighbor.second;
+                }
+        }
+    }
+    
+    return answer;
+}
